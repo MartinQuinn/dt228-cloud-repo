5edbdc02781b182ebb693c32bb824d8a70beab60
martinquinn <martoq28@gmail.com>
2015-09-30 14:55:42 +0000

    Second if statement

--- Q19.py
+++ Q19.py
@@ -2,10 +2,13 @@
 
 Word = raw_input("Please Enter a Word!\n")
 x=0
-  
+
 for i in range (len(word)/2)
   if(word[x])==(word[len(word)-x-1]):
     x += 1
-  else: 
+  else:
     t = False
+
+  if x == (len(word)/2):
+    t = True
     
